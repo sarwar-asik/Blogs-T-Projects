4904c4a76482d98ba2120f2bbc7845e5a76c5388
sarwar-asik <shasik2021@gmail.com>
2023-01-01 15:45:05 +0600

    useReducer

--- src/components/shared/Skaleton/ProjectSkelten.tsx
+++ src/components/shared/Skaleton/ProjectSkelten.tsx
@@ -13,33 +13,25 @@ const ProjectSkelten = ({ projects: [] }) => {
           <div className="flex flex-col justify-center items-center bg-gray-300 rounded dark:bg-gray-700 relative projectBg">
             <div className=" flex gap-10">
               <button className="h-10 border-[1px] text-slate-600 font-bold  border-slate-600 bg-gray-200 rounded-md dark:bg-gray-700 w-20 mb-4 shadow animate-pulse">
-                Live{" "}
+                Live
               </button>
               <button className="h-10 text-slate-600 font-bold border-[1px] border-slate-600 bg-gray-200 rounded-md dark:bg-gray-700 w-20 mb-4 shadow animate-pulse">
-                Source{" "}
+                Source
               </button>
             </div>
 
-            <h3 className="w-[90%] mx-auto h-[5px] shadow animate-pulse bg-slate-500 mt-3">
-              {" "}
-            </h3>
-            <h3 className="w-[70%] mx-auto h-[5px] shadow animate-pulse bg-slate-500 mt-3">
-              {" "}
-            </h3>
-            <h3 className="w-[50%] mx-auto h-[5px] shadow animate-pulse bg-slate-500 mt-3">
-              {" "}
-            </h3>
+            <h3 className="w-[90%] mx-auto h-[5px] shadow animate-pulse bg-slate-500 mt-3">---</h3>
+            <h3 className="w-[70%] mx-auto h-[5px] shadow animate-pulse bg-slate-500 mt-3">--</h3>
+            <h3 className="w-[50%] mx-auto h-[5px] shadow animate-pulse bg-slate-500 mt-3">-</h3>
 
             <h1 className=" absolute left-5 bottom-5 text-slate-600 font-bold">
-              {" "}
               Content Name
             </h1>
           </div>
         );
       })}
 
-      {/* <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-<div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div> */}
+ 
     </div>
   );
 };
